b4b4e47dfaa4bc0ca7bb2a97e7f6f18dced3b3c5
slaymi <tt208741@gmail.com>
2024-05-11 23:13:26 +0600

    Init

---
+++ cmake-build-relwithdebinfo-wsl/CMakeFiles/halal.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/halal.dir/src/main.cpp.o"
+  "CMakeFiles/halal.dir/src/main.cpp.o.d"
+  "halal"
+  "halal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/halal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
